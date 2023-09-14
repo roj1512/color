12964022c94763bd4453b7ee7032ec1a6b1c6197
Roj <ez@roj.im>
2023-09-14 20:57:06 +0300

    Fix regex

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ await initialize(Deno.readFile("./svg2png_wasm_bg.wasm"));
 
 function getSvg(color: string) {
   color = color.replace(/^#/, "");
-  if (!/^([a-z]{6}|[a-z]{8}|[a-z]{4}|[a-z]{3})$/i.test(color)) {
+  if (!/^([a-z0-9]{6}|[a-z0-9]{8}|[a-z0-9]{4}|[a-z0-9]{3})$/i.test(color)) {
     color = "000000";
   }
   return `<svg width="512" height="512" viewBox="0 0 512 512" fill="none" xmlns="http://www.w3.org/2000/svg">
